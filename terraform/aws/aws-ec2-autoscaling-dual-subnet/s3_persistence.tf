e8eb09f29985afb917de0e920b08c7f8e585106a
Cameron Stokes <cameron@cameronstokes.com>
2024-02-19 22:21:34 -0800

    fix issues (mainly s3 path) in restoring

--- terraform/aws/aws-ec2-autoscaling-dual-subnet/s3_persistence.tf
+++ terraform/aws/aws-ec2-autoscaling-dual-subnet/s3_persistence.tf
@@ -1,6 +1,6 @@
 locals {
-  routes_file_s3_basepath = "s3://${aws_s3_bucket.routes.bucket}"
-  app_name = "aws"
+  routes_file_s3_basepath        = "s3://${aws_s3_bucket.routes.bucket}"
+  app_name                       = "aws"
   routes_file_s3_instance_prefix = "tailscale-appconnector-routes/app-${local.app_name}"
 
   script_routes_restore = <<-EOT
@@ -26,17 +26,20 @@ locals {
 
     AWS_TOKEN=\`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"\`
     AWS_ZONE_ID=\`curl -s -H "X-aws-ec2-metadata-token: \$AWS_TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone-id\`
-    FILENAME=\$AWS_ZONE_ID/
+    AWS_REGION=\`echo \$AWS_ZONE_ID | cut -d'-' -f1\`
+    S3_OBJECT_FULLPATH=${local.routes_file_s3_basepath}/${local.routes_file_s3_instance_prefix}/\$AWS_REGION/
 
     # skip the first two lines which are the device's own tailscale addresses
     tailscale status --json | jq -r .Self.AllowedIPs[] | tail -n +3 > \$ROUTES_TO_RESTORE_DIR/initial-routes-from-userdata.txt
 
     aws s3 cp \
       --recursive \
-      ${local.routes_file_s3_basepath}/${local.routes_file_s3_instance_prefix}/\$FILENAME \
+      \$S3_OBJECT_FULLPATH \
       \$ROUTES_TO_RESTORE_DIR
 
     ROUTES=\$(cat \$ROUTES_TO_RESTORE_DIR/* | tr '\n' ',' | sed 's/[,]$//')
+    ROUTES_COUNT=\$(cat \$ROUTES_TO_RESTORE_DIR/* | wc -l)
+    echo \`date -u +"%Y-%m-%dT%H:%M:%SZ"\`": restoring [\$ROUTES_COUNT] routes from \$S3_OBJECT_FULLPATH"
     tailscale set --advertise-routes=\$ROUTES
 
     EOF
@@ -62,17 +65,20 @@ locals {
     #!/bin/bash
 
     AWS_TOKEN=\`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"\`
-    AWS_ZONE_ID=\`curl -H "X-aws-ec2-metadata-token: \$AWS_TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone-id\`
-    AWS_AMI_LAUNCH_INDEX=\`curl -H "X-aws-ec2-metadata-token: \$AWS_TOKEN" http://169.254.169.254/latest/meta-data/ami-launch-index\`
-    FILENAME=\$AWS_ZONE_ID/ami-launch-index-\$AWS_AMI_LAUNCH_INDEX.txt
+    AWS_ZONE_ID=\`curl -s -H "X-aws-ec2-metadata-token: \$AWS_TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone-id\`
+    AWS_REGION=\`echo \$AWS_ZONE_ID | cut -d'-' -f1\`
+    AWS_AMI_LAUNCH_INDEX=\`curl -s -H "X-aws-ec2-metadata-token: \$AWS_TOKEN" http://169.254.169.254/latest/meta-data/ami-launch-index\`
+    S3_OBJECT_FULLPATH=${local.routes_file_s3_basepath}/${local.routes_file_s3_instance_prefix}/\$AWS_REGION/\$AWS_ZONE_ID-ami-launch-index-\$AWS_AMI_LAUNCH_INDEX.txt
 
-    echo -n \`date -u +"%Y-%m-%dT%H:%M:%SZ"\`': '
+    tailscale status --json | jq -r .Self.AllowedIPs[] | tail -n +3 > routes.tmp
+    ROUTES_COUNT=\$(cat routes.tmp | wc -l)
+
+    echo \`date -u +"%Y-%m-%dT%H:%M:%SZ"\`": saving [\$ROUTES_COUNT] routes to \$S3_OBJECT_FULLPATH"
 
     # skip the first two lines which are the device's own tailscale addresses
-    tailscale status --json | jq -r .Self.AllowedIPs[] | tail -n +3 | \
-      aws s3 cp \
-      - \
-      ${local.routes_file_s3_basepath}/${local.routes_file_s3_instance_prefix}/\$FILENAME
+    aws s3 cp \
+      routes.tmp \
+      \$S3_OBJECT_FULLPATH
 
     EOF
 
