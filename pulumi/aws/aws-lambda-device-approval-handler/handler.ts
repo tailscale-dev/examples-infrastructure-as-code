0b2f88ad183a8ad8fc77bbb31ec73b1f31475bb1
Cameron Stokes <cameron@cameronstokes.com>
2024-06-21 11:38:06 -0700

    count errors

--- pulumi/aws/aws-lambda-device-approval-handler/handler.ts
+++ pulumi/aws/aws-lambda-device-approval-handler/handler.ts
@@ -11,38 +11,59 @@ interface TailnetEvent {
 
 export const lambdaHandler = async (ev: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // TODO: https://tailscale.com/kb/1213/webhooks#verifying-an-event-signature
+
+    console.log(`Received event: ${JSON.stringify(ev)}`);
+
+    let successOrIgnoredCount = 0;
+    let errorCount = 0;
     try {
-        let tailnetEvents: TailnetEvent[] = JSON.parse(ev.body!);
-        tailnetEvents.forEach((event) => {
+        const tailnetEvents: TailnetEvent[] = JSON.parse(ev.body!);
+        const errors = tailnetEvents.map((event): Error | null => {
+            // https://tailscale.com/kb/1213/webhooks#events
             switch (event.type) {
                 case "nodeNeedsApproval":
-                    eventFunctions.nodeNeedsApproval(event);
-                    break;
+                    return nodeNeedsApprovalHandler(event);
                 default:
-                    unhandledHandler(event);
+                    return unhandledHandler(event);
             }
         });
-        return {
-            statusCode: 204,
-            body: JSON.stringify({ message: "ok" }),
-        };
+        successOrIgnoredCount = errors.filter(er => er == null).length;
+        errorCount = errors.filter(er => er != null).length;
+
+        let statusCode = 200;
+        if (errorCount > 0) {
+            statusCode = 500;
+        }
+        console.log(`successfully processed or ignored count: [${successOrIgnoredCount}], error count: [${errorCount}]`);
+        return generateResponseBody(statusCode, ev, successOrIgnoredCount, errorCount);
     } catch (err) {
         console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'An error occurred.' }),
-        };
+        return generateResponseBody(500, ev, successOrIgnoredCount, errorCount);
     }
 };
 
-const nodeNeedsApprovalHandler = function (event: TailnetEvent) {
+const generateResponseBody = function (statusCode: number, ev: APIGatewayProxyEvent, successOrIgnoredCount: number, errorCount: number): APIGatewayProxyResult {
+    return {
+        statusCode: statusCode,
+        body: JSON.stringify({
+            message: (statusCode == 200 ? "ok" : "An error occurred."),
+            // requestId: ev.requestContext.requestId, // TODO: This requestId doesn't match what's in the lambda logs.
+            eventCounts: {
+                successOrIgnored: successOrIgnoredCount,
+                error: errorCount,
+            },
+        }),
+    };
+}
+
+const nodeNeedsApprovalHandler = function (event: TailnetEvent): Error | null {
     console.log(`Handling event type [${event.type}]`);
+    return null;
+    // console.log(`returning fake error`);
+    // return new Error(`Fake error occurred for  [${event.type}]`);
 }
 
-const unhandledHandler = function (event: TailnetEvent) {
+const unhandledHandler = function (event: TailnetEvent): null {
     console.log(`Skipping unhandled event type [${event.type}]`);
+    return null;
 }
-
-const eventFunctions = {
-    "nodeNeedsApproval": nodeNeedsApprovalHandler,
-};
