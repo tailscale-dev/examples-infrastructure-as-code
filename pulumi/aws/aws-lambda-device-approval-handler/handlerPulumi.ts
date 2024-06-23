0e3197f2113e942064e7641ee8107c41920665df
Cameron Stokes <cameron@cameronstokes.com>
2024-06-23 16:39:11 -0700

    add pulumi config for oauth client env vars

--- pulumi/aws/aws-lambda-device-approval-handler/handlerPulumi.ts
+++ pulumi/aws/aws-lambda-device-approval-handler/handlerPulumi.ts
@@ -1,9 +1,19 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+
 import * as handler from "./handler";
 
+const pulumiConfig = new pulumi.Config();
 
 export const getPulumiHandler = (name: string) => {
     return new aws.lambda.CallbackFunction(name, {
+        environment: {
+            variables: {
+                [handler.ENV_TAILSCALE_OAUTH_CLIENT_ID]: pulumiConfig.require("tailscaleOauthClientId"),
+                [handler.ENV_TAILSCALE_OAUTH_CLIENT_SECRET]: pulumiConfig.requireSecret("tailscaleOauthClientSecret"),
+            },
+        },
+        runtime: "nodejs20.x",
         callback: async (ev: any, ctx) => {
             return handler.lambdaHandler(ev);
         },
