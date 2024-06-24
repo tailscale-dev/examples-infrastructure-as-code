27be0070d33b707360ee733e0b28bb40611c579b
Cameron Stokes <cameron@cameronstokes.com>
2024-06-23 17:32:04 -0700

    cleanup and error handling

--- pulumi/aws/aws-lambda-device-approval-handler/handler.ts
+++ pulumi/aws/aws-lambda-device-approval-handler/handler.ts
@@ -1,32 +1,9 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import querystring = require("querystring"); // TODO: not necessary?
-
-interface TailnetEvent {
-    timestamp: string;
-    version: number;
-    type: string;
-    tailnet: string;
-    message: string;
-    data: any
-};
-
-interface TailnetEventDeviceData {
-    nodeID: string;
-    deviceName: string;
-    managedBy: string;
-    actor: string;
-    url: string;
-};
-
-interface ProcessingResult {
-    event: TailnetEvent;
-    result: "SUCCESS" | "ERROR" | "IGNORED";
-    error: Error | null;
-}
 
 export async function lambdaHandler(ev: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
     // TODO: https://tailscale.com/kb/1213/webhooks#verifying-an-event-signature
-    console.log(`Received event: ${JSON.stringify(ev)}`);
+    // console.log(`Received event: ${JSON.stringify(ev)}`); // TODO: add verbose logging flag?
+
     let successCount = 0;
     let errorCount = 0;
     let ignoreCount = 0;
@@ -38,13 +15,18 @@ export async function lambdaHandler(ev: APIGatewayProxyEvent): Promise<APIGatewa
         const tailnetEvents: TailnetEvent[] = JSON.parse(decodedBody!);
         const results: ProcessingResult[] = [];
         for (const event of tailnetEvents) {
-            switch (event.type) { // https://tailscale.com/kb/1213/webhooks#events
-                case "nodeNeedsApproval":
-                    results.push(await nodeNeedsApprovalHandler(event));
-                    break;
-                default:
-                    results.push(await unhandledHandler(event));
-                    break;
+            try {
+                switch (event.type) { // https://tailscale.com/kb/1213/webhooks#events
+                    case "nodeNeedsApproval":
+                        results.push(await nodeNeedsApprovalHandler(event));
+                        break;
+                    default:
+                        results.push(await unhandledHandler(event));
+                        break;
+                }
+            }
+            catch (err: any) {
+                results.push({ event: event, result: "ERROR", error: err, } as ProcessingResult);
             }
         }
         results.forEach(it => {
@@ -87,23 +69,17 @@ function generateResponseBody(statusCode: number, ev: APIGatewayProxyEvent, succ
 
 async function unhandledHandler(event: TailnetEvent): Promise<ProcessingResult> {
     console.log(`Ignoring event type [${event.type}]`);
-    return {
-        event: event,
-        result: "IGNORED",
-    } as ProcessingResult;
+    return { event: event, result: "IGNORED", } as ProcessingResult;
 }
 
 async function nodeNeedsApprovalHandler(event: TailnetEvent): Promise<ProcessingResult> {
     try {
         console.log(`Handling event type [${event.type}]`);
         await approveDevice(event.data as TailnetEventDeviceData);
+        return { event: event, result: "SUCCESS", } as ProcessingResult;
+    } catch (err: any) {
+        return { event: event, result: "ERROR", error: err, } as ProcessingResult;
     }
-    catch (err: any) {
-        console.log(`Caught error [${err}]`);
-        console.log(err.stack);
-        return { result: "IGNORED", event: event, error: err, } as ProcessingResult;
-    }
-    return { result: "SUCCESS", event: event, } as ProcessingResult;
 }
 
 export const ENV_TAILSCALE_OAUTH_CLIENT_ID = "OAUTH_CLIENT_ID";
@@ -111,25 +87,17 @@ export const ENV_TAILSCALE_OAUTH_CLIENT_SECRET = "OAUTH_CLIENT_SECRET";
 const TAILSCALE_CONTROL_URL = "https://login.tailscale.com";
 
 // https://github.com/tailscale/tailscale/blob/main/publicapi/device.md#authorize-device
-async function approveDevice(device: TailnetEventDeviceData): Promise<Error | null> {
-    try {
-        console.log(`Approving device [${device.nodeID}:${device.deviceName}]`);
-        const data = await makeAuthenticatedPost(`${TAILSCALE_CONTROL_URL}/api/v2/device/${device.nodeID}/authorized`, JSON.stringify({ "authorized": true }));
-        const json = await data.json();
-        console.log(`device approval response [${JSON.stringify(json)}]`);
-        const ok = await data.ok;
-        if (!ok) {
-            return Error(`Failed to approve device [${device.nodeID}:${device.deviceName}]`);
-        }
-    } catch (err: any) {
-        console.log(`Caught error [${err}]`);
-        console.log(err.stack);
-        return err;
+async function approveDevice(device: TailnetEventDeviceData) {
+    console.log(`Approving device [${device.nodeID}:${device.deviceName}]`);
+    const data = await makeAuthenticatedPost(`${TAILSCALE_CONTROL_URL}/api/v2/device/${device.nodeID}/authorized`, JSON.stringify({ "authorized": true }));
+    const json = await data.json();
+    console.log(`device approval response [${JSON.stringify(json)}]`);
+    if (!data.ok) {
+        throw new Error(`Failed to approve device [${device.nodeID}:${device.deviceName}]`);
     }
-
-    return null;
 }
 
+// https://tailscale.com/kb/1215/oauth-clients
 export async function getAccessToken(): Promise<any> {
     const oauthClientId = process.env[ENV_TAILSCALE_OAUTH_CLIENT_ID];
     const oauthClientSecret = process.env[ENV_TAILSCALE_OAUTH_CLIENT_SECRET];
@@ -137,17 +105,10 @@ export async function getAccessToken(): Promise<any> {
         throw new Error(`Missing required environment variables ${ENV_TAILSCALE_OAUTH_CLIENT_ID} and ${ENV_TAILSCALE_OAUTH_CLIENT_SECRET}. See https://tailscale.com/kb/1215/oauth-clients.`);
     }
 
-    const data = querystring.stringify({
-        client_id: oauthClientId,
-        client_secret: oauthClientSecret,
-    });
-
     const options: RequestInit = {
         method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: data,
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `client_id=${oauthClientId}&client_secret=${oauthClientSecret}`,
     };
 
     // console.log(`getting access token`);
@@ -160,9 +121,7 @@ const makeAuthenticatedPost = async function (url: string, body: string): Promis
 
     const options: RequestInit = {
         method: "POST",
-        headers: {
-            "Authorization": `Bearer ${result.access_token}`
-        },
+        headers: { "Authorization": `Bearer ${result.access_token}` },
         body: body,
     };
 
@@ -170,6 +129,29 @@ const makeAuthenticatedPost = async function (url: string, body: string): Promis
 }
 
 async function httpsRequest(url: string, options: any): Promise<Response> {
-    console.log(`Making HTTP request to [${url}] with options [${JSON.stringify(options)}]`); // TODO: add verbose logging flag?
+    // console.log(`Making HTTP request to [${url}] with options [${JSON.stringify(options)}]`); // TODO: add verbose logging flag?
     return await fetch(url, options);
+}
+
+interface TailnetEvent {
+    timestamp: string;
+    version: number;
+    type: string;
+    tailnet: string;
+    message: string;
+    data: any
+};
+
+interface TailnetEventDeviceData {
+    nodeID: string;
+    deviceName: string;
+    managedBy: string;
+    actor: string;
+    url: string;
+};
+
+interface ProcessingResult {
+    event: TailnetEvent;
+    result: "SUCCESS" | "ERROR" | "IGNORED";
+    error: Error | null;
 }
