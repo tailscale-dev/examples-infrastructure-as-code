2cf716b92e4fcbb77f9c17395dbbc24dddd2ce7f
Cameron Stokes <cameron@cameronstokes.com>
2024-06-23 21:09:00 -0700

    remove unnecessary `GET /` route

--- pulumi/aws/aws-lambda-device-approval-handler/index.ts
+++ pulumi/aws/aws-lambda-device-approval-handler/index.ts
@@ -1,7 +1,5 @@
-import * as path from 'path';
 import * as apigateway from "@pulumi/aws-apigateway";
-import * as aws from "@pulumi/aws";
-
+import * as path from 'path';
 import * as handler from "./handlerPulumi";
 
 const name = `example-${path.basename(process.cwd())}`;
@@ -10,16 +8,6 @@ const api = new apigateway.RestAPI(name, {
     stageName: "tailscale-device-approval",
     binaryMediaTypes: ["application/json"],
     routes: [
-        {
-            path: "/",
-            method: "GET",
-            eventHandler: new aws.lambda.CallbackFunction(name, {
-                callback: async (ev: any, ctx) => {
-                    console.log(`Not handling GET from ${ev.requestContext.identity.sourceIp}`);
-                    return { statusCode: 500 };
-                },
-            }),
-        },
         {
             path: "/",
             method: "POST",
