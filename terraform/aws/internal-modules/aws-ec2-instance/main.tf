3aea8d61e575f0e1d300859b3ecce20e188340a6
Cameron Stokes <cameron@cameronstokes.com>
2024-08-17 12:32:09 -0700

    update aws-ec2-autoscaling-session-recorder

--- terraform/aws/internal-modules/aws-ec2-instance/main.tf
+++ terraform/aws/internal-modules/aws-ec2-instance/main.tf
@@ -1,9 +1,9 @@
 module "tailscale_install_scripts" {
   source = "../../../internal-modules/tailscale-install-scripts"
 
-  tailscale_auth_key            = var.tailscale_auth_key
-  tailscale_hostname            = var.tailscale_hostname
-  tailscale_set_preferences     = var.tailscale_set_preferences
+  tailscale_auth_key        = var.tailscale_auth_key
+  tailscale_hostname        = var.tailscale_hostname
+  tailscale_set_preferences = var.tailscale_set_preferences
 
   additional_before_scripts = var.additional_before_scripts
   additional_after_scripts  = var.additional_after_scripts
