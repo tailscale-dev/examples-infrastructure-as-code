0fd338c48a8dc5d1c313e6e5e79f674af8592d0d
Raj Singh <raj@tailscale.com>
2025-04-19 21:25:08 -0500

    add

--- terraform/google/google-gke-dataplane-v2/main.tf
+++ terraform/google/google-gke-dataplane-v2/main.tf
@@ -90,7 +90,7 @@ resource "google_container_cluster" "primary" {
 
   # Private cluster config
   private_cluster_config {
-    enable_private_nodes    = true
+    enable_private_nodes    = false
     enable_private_endpoint = false
     master_ipv4_cidr_block  = "172.16.0.0/28"
   }
