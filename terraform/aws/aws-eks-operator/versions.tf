2982944bb09c719f3c1004253a4afd93338b8af5
Cameron Stokes <cameron@tailscale.com>
2025-12-23 07:35:56 -0800

    remove beta from api_version for aws auth call

--- terraform/aws/aws-eks-operator/versions.tf
+++ terraform/aws/aws-eks-operator/versions.tf
@@ -31,7 +31,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1"
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
@@ -44,7 +44,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
     exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
+      api_version = "client.authentication.k8s.io/v1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
